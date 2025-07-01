24cea7eaacee51aa34ced483f4650717589d53aa
LahariBonthu <laharibonthu1882@gmail.com>
2025-07-02 00:42:19 +0530

    Updated LandingPage and AboutUs

--- Frontend/src/pages/LandingPage.jsx
+++ Frontend/src/pages/LandingPage.jsx
@@ -1,35 +1,21 @@
 // src/pages/LandingPage.js
 import React from 'react';
 import HeroSection from '../components/LandingPage/HeroSection'; // Default import
-import FeatureCard from '../components/LandingPage/FeatureCard'; // Default import
+import SlideShow from '../components/LandingPage/FeatureCard'; // Default import
 import BenefitsSection from '../components/LandingPage/BenefitsSection'; // Default import
+import AboutUs from './AboutUs';
 
 const LandingPage = () => {
   return (
     <div className="landing-page">
       <HeroSection />
-      
-      <section className="features">
-        <div className="container">
-          <h2>How It Works</h2>
-          <div className="features-grid">
-            <FeatureCard 
-              title="Google Fit Integration" 
-              description="Seamlessly connect your health data from Google Fit"
-            />
-            <FeatureCard 
-              title="Dynamic Premium Adjustment" 
-              description="Your insurance costs update based on real-time health metrics"
-            />
-            <FeatureCard 
-              title="Risk Clustering" 
-              description="Personalized risk assessment (High/Medium/Low)"
-            />
-          </div>
-        </div>
-      </section>
-      
+
+
+      <SlideShow />
+
       <BenefitsSection />
+
+      <AboutUs />
     </div>
   );
 };
